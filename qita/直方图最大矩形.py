6a873bd73f297b52df98a0178c5a18d4da18d17e
jwf-ai <jianwenfu@foxmail.com>
2018-09-05 19:37:14 +0800

    update

--- qita/直方图最大矩形.py
+++ qita/直方图最大矩形.py
@@ -1,24 +1,28 @@
 # encoding: utf-8
 
-def largest_rectangle_area(arr):
+def largest_rectangle_area(A):
+    n = len(A)
+
     stack = []
     max_area = 0
-    for i in range(len(arr)):
-
-        while len(stack) !=0 and arr[i] <= arr[stack[-1]]:
-            index = stack[-1]
-            height = arr[index]
-            stack.pop()
-
-            if len(stack) == 0:
-                width = i
-            else:
-                width = i - stack[-1] - 1
+    i = 0
+    while i < n:
+        if len(stack) == 0 or A[i] >= A[stack[-1]]:
+            stack.append(i)
+            i += 1
+        else:
+            index = stack.pop()
+            height = A[index]
+            width = i - stack[-1] - 1 if len(stack) != 0 else i
             area = height * width
-            if area > max_area:
-                max_area = area
+            max_area = max(max_area, area)
+    while stack:
+        index = stack.pop()
+        height = A[index]
+        width = i - stack[-1] - 1 if len(stack) != 0 else i
+        area = height * width
+        max_area = max(max_area, area)
 
-        stack.append(i)
     return max_area
 
 arr = [2,1,5,6,2,3]
